45e15ff3464b9652437e5dadfa92908e4e16bdfa
Terri Jaskolka <jas20003@byui.edu>
2022-06-28 15:54:35 -0400

    added swagger defs to swagger.js and routes

--- routes/recipe-routes.js
+++ routes/recipe-routes.js
@@ -3,24 +3,38 @@ const recipes = require('../controller/recipe');
 const validation = require('../validation');
 
 // Create a new recipe
-routes.post('/', validation.addNewRecipe, recipes.create);
+routes.post('/', validation.addNewRecipe, recipes.create
+    // #swagger.tags = ['Recipes']
+);
 
 // Retrieve all recipes by key words
-routes.get('/keywords', recipes.findByKeyWords);
+routes.get('/keywords', recipes.findByKeyWords
+    // #swagger.tags = ['Recipes']
+);
 
 // Retrieve all recipes by user posted
-routes.get('/userPosted/:userPostedId', recipes.findByUserPosted);
+routes.get('/userPosted/:userPostedId', recipes.findByUserPosted
+    // #swagger.tags = ['Recipes']
+);
 
 // Retrieve a single recipe with id
-routes.get('/:recipe_id', recipes.findOne);
+routes.get('/:recipe_id', recipes.findOne
+    // #swagger.tags = ['Recipes']
+);
 
 // Retrive all recipes in database
-routes.get('/', recipes.findAll);
+routes.get('/', recipes.findAll
+    // #swagger.tags = ['Recipes']
+);
 
 // Update a recipe with id
-routes.put('/:id', validation.updateOneRecipe, recipes.update);
+routes.put('/:id', validation.updateOneRecipe, recipes.update
+    // #swagger.tags = ['Recipes']
+);
 
 // Delete a recipe with id
-routes.delete('/:id', recipes.delete);
+routes.delete('/:id', recipes.delete
+    // #swagger.tags = ['Recipes']
+);
 
 module.exports = routes;
--- routes/review-routes.js
+++ routes/review-routes.js
@@ -3,7 +3,10 @@ const reviews = require('../controller/review.js');
 const validation = require('../validation');
 
 // Create a new recipe
-routes.post('/:recipe_id/:user_id', validation.addNewReview, reviews.create);
+routes.post('/:recipe_id/:user_id', validation.addNewReview, reviews.create
+    // #swagger.tags = ['Reviews']
+    // #swagger.description = 'Add a review'
+);
 // Retrieve all recipes by user posted
 //routes.get('/userPosted/:userPostedId', recipes.findByUserPosted);
 
@@ -11,7 +14,16 @@ routes.post('/:recipe_id/:user_id', validation.addNewReview, reviews.create);
 //routes.get('/:recipe_id', recipes.findOne);
 
 // GET ALL REVIEWS
-routes.get('/', reviews.findAll);
+routes.get('/', reviews.findAll
+    // #swagger.tags = ['Reviews']
+    // #swagger.description = 'Get all reviews'
+    /* #swagger.parameters['recipe_id'] = {
+        in: 'path',
+        description: 'Input recipe ID',
+        required: true,
+        type: 'string'
+    }*/
+);
 
 // Update a recipe with id
 //routes.put('/:id', validation.updateOneRecipe, recipes.update);
