cdf3ed5a2f99ace8c31d5ec40a07b35b0df42368
Terri Jaskolka <jas20003@byui.edu>
2022-07-02 05:43:13 -0400

    attempting update for documentation of recipe-routes

--- routes/recipe-routes.js
+++ routes/recipe-routes.js
@@ -2,8 +2,17 @@ const routes = require('express').Router();
 const recipes = require('../controller/recipe');
 const validation = require('../validation');
 
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+      res.redirect('/auth/home');
+    } else {
+      next();
+    }
+  };
+
+  
 // Create a new recipe
-routes.post('/', validation.addNewRecipe, recipes.create
+routes.post('/',authCheck, validation.addNewRecipe, recipes.create
     // #swagger.tags = ['Recipes']
     // #swagger.summary = 'Add a recipe to the database'
     /* #swagger.parameters['obj'] = {
@@ -43,6 +52,25 @@ routes.get('/:recipe_id', recipes.findOne
     // #swagger.tags = ['Recipes']
 );
 
+// Retrieve all recipes by key words
+routes.get('/keywords', authCheck, recipes.findByKeyWords
+    // #swagger.tags = ['Recipes']
+);
+
+// Retrieve all recipes by ingredients
+routes.get('/ingredients', recipes.findByIngredients
+    // #swagger.tags = ['Recipes']
+);
+
+// Retrieve all recipes by user posted
+routes.get('/userPosted/:userPostedId', authCheck, recipes.findByUserPosted
+    // #swagger.tags = ['Recipes']
+);
+
+// Retrieve a single recipe with id
+routes.get('/:recipe_id', authCheck, recipes.findOne
+    // #swagger.tags = ['Recipes']
+);
 // Retrive all recipes in database
 routes.get('/', recipes.findAll
     // #swagger.tags = ['Recipes']
